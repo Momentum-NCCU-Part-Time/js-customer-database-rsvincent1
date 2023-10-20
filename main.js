1d586e9438de2868372a1a20b8e220063ac7a133
rsvincent <rvincent87@gmail.com>
2023-10-20 16:25:12 -0400

    first and last name displayed

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+let customerContainer = document.querySelector('.container')
 
 function customerDirectoryCard(customer){
-    return `<div class="firstName"> ${customer.name.first} <div>`
+    let customerCard = document.createElement('div');
+    let customerName = document.createElement('h2');
+    customerName.innerText = `${customer.name.first} ${customer.name.last}`
+    customerCard.appendChild(customerName)
+    customerContainer.appendChild(customerCard)
 }
 let database = customers.map((customer) => customerDirectoryCard(customer))
-document.querySelector('.container').innerHTML = database;
+

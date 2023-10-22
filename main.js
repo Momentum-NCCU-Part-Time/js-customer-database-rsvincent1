87afccfdbebae81dcc3c716c4771bdbca1f85b29
rsvincent <rvincent87@gmail.com>
2023-10-22 10:35:34 -0400

    directory created

--- main.js
+++ main.js
@@ -1,29 +1,32 @@
 let customerContainer = document.querySelector('.container')
 
 function customerDirectoryCard(customer) {
-    let customerTitle = document.createElement('div');
+    let customerImage = document.createElement('img');
     let customerCard = document.createElement('div');
     let customerName = document.createElement('h2');
     let customerEmail = document.createElement('div');
     let customerAddress = document.createElement('div');
     let customerLocation = document.createElement('div');
-    let customerDOB = document.createElement('div');
+    let customerDob = document.createElement('div');
+    let customerEst = document.createElement('div');
 
-
+    customerImage.src = `${customer.picture.large}`
     customerName.innerText = `${customer.name.title} ${customer.name.first} ${customer.name.last}`;
     customerEmail.innerText = `${customer.email}`
     customerAddress.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
     customerLocation.innerText = `${customer.location.city} ${customer.location.state} ${customer.location.postcode}`;
-    customerDOB.innerText = `DOB:${customer.dob.date}`
-
+    customerDob.innerText = `DOB:${customer.dob.date}`;
+    customerEst.innerHTML = `Customer since: ${customer.registered.date}`
 
 
     customerContainer.appendChild(customerCard);
+    customerCard.append(customerImage)
     customerCard.appendChild(customerName);
     customerCard.appendChild(customerEmail);
     customerCard.appendChild(customerAddress);
     customerCard.appendChild(customerLocation);
-    customerCard.appendChild(customerDOB);
+    customerCard.appendChild(customerDob);
+    customerCard.appendChild(customerEst);
 
 }
 let database = customers.map((customer) => customerDirectoryCard(customer))
